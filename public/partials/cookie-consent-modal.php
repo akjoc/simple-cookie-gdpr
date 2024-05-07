932bbb2b286412a611616d24711f894f01f1be70
Aman Joshi <47720503+akjoc@users.noreply.github.com>
2024-05-07 14:34:34 +0530

    Update cookie-consent-modal.php

--- public/partials/cookie-consent-modal.php
+++ public/partials/cookie-consent-modal.php
@@ -9,55 +9,61 @@ function simple_cookie_gdpr_render_modal_popup() {
     $cookies = isset($options['cookies']) ? $options['cookies'] : [];
     ?>
     <div class="modal" id="arky_cookies_Modal" tabindex="-1" aria-labelledby="exampleModalLabel" style="display: none;" aria-modal="true" role="dialog">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel" style="font-family: inherit; font-size: 16px; margin-bottom: 15px; margin: 10px 0;"><?php echo esc_html($modal_title); ?></h5>
-                    <button type="button" class="close" onclick="closeDisclaimerModal()" aria-label="Close">
-                        <span aria-hidden="true">X</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p style="font-family: inherit; font-size: 12px; margin-bottom: 15px; margin: 0 0 12px 0;"><?php echo esc_html($modal_description); ?></p>
-                    <div class="cookies_data_modal">
-                        <div id="cookies_accordion">
-                            <?php foreach ($cookies as $index => $cookie): ?>
-                                <div class="card mb-2">
-                                    <details>
-                                        <summary>
-                                            <div class="card-header" id="heading<?php echo esc_attr($index); ?>">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel" style="font-family: inherit; font-size: 16px; margin-bottom: 15px; margin: 10px 0;"><?php echo esc_html($modal_title); ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="closeDisclaimerModal()">
+                    <span aria-hidden="true">X</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p style="font-family: inherit; font-size: 12px; margin-bottom: 15px; margin: 0 0 12px 0;"><?php echo esc_html($modal_description); ?></p>
+
+                <div class="cookies_data_modal">
+                    <div id="cookies_accordion">
+                        <?php foreach ($cookies as $index => $cookie): ?>
+                            <div class="card mb-2">
+                                <details>
+                                    <summary>
+                                        <div class="card-header" id="heading<?php echo $index; ?>">
                                                 <div class="mb-0 model-text-toggle">
                                                     <div class="w-100">
-                                                        <span class="model-click-button collapsed">
+                                                        <a class="  model-click-button collapsed" data-toggle="collapse" data-target="#cookies_collapse<?php echo $index; ?>" aria-expanded="false" aria-controls="cookies_collapse<?php echo $index; ?>">
                                                             <?php echo esc_html($cookie['name']); ?>
-                                                        </span>
+                                                        </a>
                                                     </div>
+
                                                     <div class="cookies_sec_tgl model-text-toggle">
                                                         <label class="toggle">
-                                                            <input type="checkbox" <?php echo !empty($cookie['status']) ? 'checked' : ''; ?>>
+                                                            <input type="checkbox" checked <?php echo !empty($cookie['status']) ? 'checked' : ''; ?> hidden>
                                                             <span class="circle"></span>
                                                         </label>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </summary>
-                                        <div class="card-body">
-                                            <p class="mb-0 fs-5"><?php echo esc_html($cookie['description']); ?></p>
-                                        </div>
-                                    </details>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
+                                    </summary>
+                                    <div id="cookies_collapse<?php echo $index; ?>" class="collapse" aria-labelledby="heading<?php echo $index; ?>" data-parent="#cookies_accordion">
+                                            <div class="card-body">
+                                                <p class="mb-0 fs-5"><?php echo esc_html($cookie['description']); ?></p>
+                                            </div>
+                                     </div>
+                        </details>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
+
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" onclick="closeDisclaimerModal()"><?php echo esc_html($modal_close_button_text); ?></button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="closeDisclaimerModal()"><?php echo esc_html($modal_close_button_text); ?></button>
                     <button type="button" class="btn btn-primary" onclick="setCookieViaAjax()"><?php echo esc_html($modal_accept_button_text); ?></button>
                 </div>
             </div>
         </div>
     </div>
-    <?php
+</div>
+
+<?php
 }
 
 add_action('wp_footer', 'simple_cookie_gdpr_render_modal_popup');
